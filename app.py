74d22d265785cbbdda493f4c7e3107e82db72575
ROHANDEV-web <pinkidevicpr@gmail.com>
2026-02-23 15:44:20 +0530

    Fixed Render PORT binding issue

--- app.py
+++ app.py
@@ -421,6 +421,8 @@ def ads_txt():
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
+import os
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
